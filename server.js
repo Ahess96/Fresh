2af326e0dfe12ba0acb8438eaff38fd9fdb0cda9
Ahess96 <austinhess1996@comcast.net>
2023-06-28 13:11:21 -0700

    Revert port to port 3000

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.listen(port, function() {
   console.log(`Express app running on port ${port}`)
